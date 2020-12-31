f2bc41289c6b3f43aa8b9aa2c40ab81143e8158b
christophertcampbell <chris@christophertcampbell.com>
2020-12-30 18:20:40 -0800

    Refactor .find() for simplicity

--- 2020/src/day05/BSPSearch.java
+++ 2020/src/day05/BSPSearch.java
@@ -29,47 +29,24 @@ public class BSPSearch
      */
     public int find(String searchPattern)
     {
+        int lBound = lowerBound;
+        int uBound = upperBound;
+
         searchPattern = normalizeSearchPattern(searchPattern);
-        return doSearch(searchPattern, lowerBound, upperBound);
-    }
 
-    /**
-     * Recursive binary search based upon a search pattern of <code>R</code> and <code>L</code> characters
-     * 
-     * @param searchPattern A string composed of one or more letters indicating left (<code>L</code>) or right (<code>R</code>) in the tree
-     * @return The matching number, or <code>Integer.MIN_VALUE</code> if unsuccessful
-     */
-    private int doSearch(String searchPattern, int minNum, int maxNum)
-    {
-        int diff = maxNum - minNum;
-
-        // Base case, only one search letter left
-        if (searchPattern.length() == 1) {
-            if (diff == 0) {
-                // Bounds are the same, return it
-                return minNum;
-            } else if (diff == 1) {
-                // Bounds are sequential, return the higher or lower as appropriate
-                return searchPattern.charAt(0) == 'L' ? minNum : maxNum;
+        for (int i = 0; i < searchPattern.length(); i++)
+        {
+            int diff = uBound - lBound;
+            int shiftIfOdd = (diff % 2 == 0) ? 0 : 1; // If odd difference, shift by 1 to get to the correct side of the mid point
+            char searchChar = searchPattern.charAt(i);
+            if (searchChar == 'L') {
+                uBound = uBound - (diff / 2) - shiftIfOdd;
+            } else {
+                lBound = lBound + (diff / 2) + shiftIfOdd;
             }
-
-            // Bounds are too far apart to find an answer
-            System.out.println("Pattern: " + searchPattern + " | minNum: " + minNum + " | maxNum: " + maxNum);
-            return Integer.MIN_VALUE;
         }
 
-        // Multiple search letters left, narrow down our search by moving the bounds and continue looking
-        int shiftIfOdd = (diff % 2 == 0) ? 0 : 1; // If odd difference, shift by 1 to get to the correct side of the mid point
-        if (searchPattern.charAt(0) == 'L') {
-            // Want the left half, move the right bound in
-            maxNum = maxNum - (diff / 2) - shiftIfOdd;
-        } else {
-            // Want the right half, move the left bound in
-            minNum = minNum + (diff / 2) + shiftIfOdd;
-        }
-
-        String newSearchPattern = searchPattern.substring(1, searchPattern.length()); // Trim off the first character
-        return doSearch(newSearchPattern, minNum, maxNum); 
+        return lBound == uBound ? lBound : Integer.MIN_VALUE;
     }
 
     /**
