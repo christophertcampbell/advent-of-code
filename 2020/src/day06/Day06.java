8eec53b13d0fcc49af21d71bb2f870a30d1b2be4
christophertcampbell <chris@christophertcampbell.com>
2021-01-01 22:24:47 -0800

    Add day 6 part B solution

--- 2020/src/day06/Day06.java
+++ 2020/src/day06/Day06.java
@@ -1,6 +1,7 @@
 package day06;
 
 import java.util.HashSet;
+import java.util.HashMap;
 import utilities.FileIO;
 import utilities.Test;
 
@@ -21,6 +22,20 @@ public class Day06
     }
 
     /**
+     * Returns a sum of all groups' common characters
+     * (counts characters which are common in all lines within each group)
+     */
+    public static int partB(String[][] input)
+    {
+        int count = 0;
+        for (String[] group : input)
+        {
+            count += countCommonChars(group);
+        }
+        return count;
+    }
+
+    /**
      * Returns a count of unique chars in a group of strings
      */
     private static int countUniqueChars(String[] input)
@@ -36,6 +51,48 @@ public class Day06
         return uniqueChars.size();
     }
 
+	/**
+     * Returns a count of unique chars which all strings share in common
+     */
+    private static int countCommonChars(String[] input)
+    {
+        HashMap<Character, Integer> charCounts = new HashMap<Character, Integer>();
+
+        // Build a count of how many lines each character appears in within the group
+        for (String line : input)
+        {
+            String encounteredChars = "";
+            for (char c : line.toCharArray())
+            {
+                // Don't count the character again if we have already
+                // encountered it within this line
+                if (encounteredChars.contains(String.valueOf(c))) {
+                    continue;
+                }
+
+                encounteredChars += c;
+                if (charCounts.containsKey(c)) {
+                    charCounts.put(c, charCounts.get(c) + 1);
+                } else {
+                    charCounts.put(c, 1);
+                }
+            }
+        }
+
+        // Count how many characters are common among all lines in the group
+        int count = 0;
+        for (HashMap.Entry<Character, Integer> charCount : charCounts.entrySet())
+        {
+            // If a character's count is the same as the length of the group,
+            // then all lines within the group share the character in common
+            if (charCount.getValue() == input.length) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     /**
      * Runs the day's solutions
      */
@@ -46,6 +103,8 @@ public class Day06
 
         Test.assertEqual("Day 6 - Part A - Test input", partA(testInput), 11);
         Test.assertEqual("Day 6 - Part A - Challenge input", partA(realInput), 6662);
+        Test.assertEqual("Day 6 - Part B - Test input", partB(testInput), 6);
+        Test.assertEqual("Day 6 - Part B - Challenge input", partB(realInput), 3382);
     }
 
     public static void main(String[] args)
