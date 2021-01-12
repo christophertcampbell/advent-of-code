d2f65fa60e08d540eed116986aef1fbd7f022794
christophertcampbell <chris@christophertcampbell.com>
2021-01-11 22:58:39 -0800

    Add day 14 part B solution

--- 2020/src/day14/BitMasks.java
+++ 2020/src/day14/BitMasks.java
@@ -1,13 +1,19 @@
 package day14;
 
+import java.util.ArrayList;
+
 public class BitMasks
 {
     public long setMask = 0;
     public long unsetMask = 0;
+    public ArrayList<Long> floatingBitMasks = new ArrayList<>();
 
-    public BitMasks (long setMask, long unsetMask)
+    public BitMasks() {}
+
+    public BitMasks (long setMask, long unsetMask, ArrayList<Long> floatingBitMasks)
     {
         this.setMask = setMask;
         this.unsetMask = unsetMask;
+        this.floatingBitMasks = floatingBitMasks;
     }
 }
--- 2020/src/day14/Day14.java
+++ 2020/src/day14/Day14.java
@@ -1,5 +1,6 @@
 package day14;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -17,8 +18,8 @@ public class Day14
      */
     public static long partA(String[] input)
     {
-        BitMasks masks = new BitMasks(0,0);
-        HashMap<Integer, Long> memory = new HashMap<>();
+        BitMasks masks = new BitMasks();
+        HashMap<Long, Long> memory = new HashMap<>();
 
         for (String line : input)
         {
@@ -29,11 +30,51 @@ public class Day14
                 Matcher matcher = PATTERN_PARSE_INSTRUCTION.matcher(line);
                 if (matcher.find()) {
                     // Found a new memory allocation instruction
-                    int address = Integer.parseInt(matcher.group(1));
-                    long value = Integer.parseInt(matcher.group(2));
+                    long address = Long.parseLong(matcher.group(1));
+                    long value = Long.parseLong(matcher.group(2));
+
+                    // Apply the bit masks to the value
                     value = value | masks.setMask; // Apply the "set" bitmask
                     value = value & ~masks.unsetMask; // Apply the "unset" bitmask
                     memory.put(address, value);
+                };
+            }
+        }
+
+        return sumValues(memory);
+    }
+
+    /**
+     * Applies the appropriate bitmask to each memory address before it is written to,
+     * and returns the sum of all values in memory upon completion
+     */
+    public static long partB(String[] input)
+    {
+        BitMasks masks = new BitMasks();
+        HashMap<Long, Long> memory = new HashMap<>();
+
+        for (String line : input)
+        {
+            if (line.substring(0,4).equals("mask")) {
+                // Found a new bitmask
+                masks = parseBitMasks(line);
+            } else {
+                Matcher matcher = PATTERN_PARSE_INSTRUCTION.matcher(line);
+                if (matcher.find()) {
+                    // Found a new memory allocation instruction
+                    long address = Long.parseLong(matcher.group(1));
+                    long value = Integer.parseInt(matcher.group(2));
+
+                    // Apply the "set" bitmask to the memory address
+                    address = address | masks.setMask;
+                    
+                    // Apply all floating bitmasks to the memory address
+                    long baseAddress = address & ~masks.floatingBitMasks.get(masks.floatingBitMasks.size() - 1); // Unset all floating bits
+                    for (long floatingBitMask : masks.floatingBitMasks)
+                    {
+                        address = baseAddress | floatingBitMask;
+                        memory.put(address, value);
+                    }
                 };
             }
         }
@@ -48,27 +89,65 @@ public class Day14
     {
         long setMask = 0;
         long unsetMask = 0;
+        ArrayList<Long> floatingBits = new ArrayList<>();
+        ArrayList<Long> floatingBitMasks = new ArrayList<>();
+
+        /**
+         * Read bits (least significant bit at right) and add
+         * to the appropriate "set", "unset" or "floating" bitmask
+         */
         for (int i = 0; i < str.length(); i++)
         {
-            // Read bits from the right (least significant bit at right)
-            // and add to the appropriate "set" or "unset" mask
-            int bitIndex = str.length() - 1 - i;
+            int bitIndexFromRight = str.length() - 1 - i;
+            long bitValue = (long) Math.pow(2, bitIndexFromRight);
             if (str.charAt(i) == '1') {
-                setMask += Math.pow(2, bitIndex);
+                setMask += bitValue;
             } else if (str.charAt(i) == '0') {
-                unsetMask += Math.pow(2, bitIndex);
+                unsetMask += bitValue;
+            } else if (str.charAt(i) == 'X') {
+                floatingBits.add(bitValue);
             }
         }
-        return new BitMasks(setMask, unsetMask);
+
+        /**
+         * Create all permutations of the floating bits.
+         * Floating bits can take on either state, 0 or 1.
+         * 
+         * Do not create if the number of floating bits would make the
+         * processing too time consuming (eg: Part A test data, which
+         * doesn't need floating bit masks generated)
+         */
+        if (floatingBits.size() > 0 && floatingBits.size() < 20)
+        {
+            for (long floatingBit : floatingBits)
+            {
+                if (floatingBitMasks.size() == 0) {
+                    // Initial floating bit
+                    floatingBitMasks.add(0L);
+                    floatingBitMasks.add(floatingBit);
+                } else {
+                    int masksCount = floatingBitMasks.size();
+                    for (int x = 0; x < masksCount; x++)
+                    {
+                        // Leave the existing floating bitmasks alone (equivalent of adding the zero
+                        // state of the new bit) and duplicate the existing masks with the new bit added.
+                        // This process doubles the number of bitmasks each time.
+                        floatingBitMasks.add(floatingBitMasks.get(x) + floatingBit);
+                    }
+                }
+            }
+        }
+
+        return new BitMasks(setMask, unsetMask, floatingBitMasks);
     }
 
     /**
      * Returns the sum of all values in a HashMap
      */
-    private static long sumValues(HashMap<Integer, Long> map)
+    private static long sumValues(HashMap<Long, Long> map)
     {
         long sum = 0;
-        for (Map.Entry<Integer, Long> entry : map.entrySet())
+        for (Map.Entry<Long, Long> entry : map.entrySet())
         {
             sum += entry.getValue();
         }
@@ -80,11 +159,14 @@ public class Day14
      */
     public static void run()
     {
-        String[] testInput = FileIO.readAsStrings("2020/src/day14/Day14TestInput.txt");
+        String[] testInputPartA = FileIO.readAsStrings("2020/src/day14/Day14TestInputPartA.txt");
+        String[] testInputPartB = FileIO.readAsStrings("2020/src/day14/Day14TestInputPartB.txt");
         String[] realInput = FileIO.readAsStrings("2020/src/day14/Day14Input.txt");
 
-        Test.assertEqual("Day 14 - Part A - Test input", partA(testInput), 165);
+        Test.assertEqual("Day 14 - Part A - Test input", partA(testInputPartA), 165);
         Test.assertEqual("Day 14 - Part A - Challenge input", partA(realInput), 3059488894985L);
+        Test.assertEqual("Day 14 - Part B - Test input", partB(testInputPartB), 208);
+        Test.assertEqual("Day 14 - Part B - Challenge input", partB(realInput), 2900994392308L);
     }
 
     public static void main(String[] args)
